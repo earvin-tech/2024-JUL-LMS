a9316812417e44fdc5a988a4842e9ae9429f5765
earvin-tech <wsl@Earvin>
2024-11-13 20:55:51 +1100

    Create Student model and schema

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from init import db, ma
 def create_app():
     app = Flask(__name__)
 
+    print("Server started")
+
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 
     db.init_app(app)
