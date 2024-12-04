feb712817c3f69cdc60b90e8a14f29774394465f
earvin-tech <wsl@Earvin>
2024-12-04 15:41:19 +1100

    Implement Read operation on student entity

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask # type: ignore
 
 from init import db, ma
 from controllers.cli_controller import db_commands
+from controllers.student_controller import students_bp
 
 def create_app():
     app = Flask(__name__)
@@ -16,5 +17,6 @@ def create_app():
     ma.init_app(app)
 
     app.register_blueprint(db_commands)
+    app.register_blueprint(students_bp)
 
     return app
